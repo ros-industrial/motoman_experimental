45608237cf5616c6de17a1f5053089a2f58bcf67
Shaun Edwards <shaun.edwards@gmail.com>
2014-01-26 04:29:30 -0500

    Corrected setup.py

--- motoman_package_generator/setup.py
+++ motoman_package_generator/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['motoman_package_generator'],
-    scripts=['bin/motoman_package_generator'],
+    scripts=['src/motoman_package_generator'],
     package_dir={'': 'src'}
 )
 
