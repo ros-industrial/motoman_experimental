f90b55419c6064d50443511b43bbab1e689cae37
ros-industrial <ros-industrial.swri.org>
2014-09-08 18:14:02 -0500

    more changes to see if 7 joint commands can be sent asynchronously

--- bmda3_roscon_demo/asynchronous_moves_bmda3.py
+++ bmda3_roscon_demo/asynchronous_moves_bmda3.py
@@ -110,56 +110,65 @@ class synchronous_traj():
 
     # iterate over several configurations
     positions = self.get_state()
-    self.send_goal(positions, start, start, torso_center)
+    self.send_goal(positions, start, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, start, front, torso_center)
+    self.send_goal(positions, [], start, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, front, front, torso_center)
+    self.send_goal(positions, [], front, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, front, front_flip, torso_center)
+    self.send_goal(positions, front, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, front_flip, front_flip, torso_center)
+    self.send_goal(positions, [], front_flip, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, front_flip, right_shoulder, torso_center)
+    self.send_goal(positions, front_flip, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, left_shoulder, right_shoulder, torso_center)
+    self.send_goal(positions, [], right_shoulder, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, behind_head, right_shoulder, torso_center)
+    self.send_goal(positions, left_shoulder, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, behind_head, behind_head, torso_center)
+    self.send_goal(positions, behind_head, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, behind_head, right_hip, torso_center)
+    self.send_goal(positions, [], behind_head, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, left_hip, right_hip, torso_center)
+    self.send_goal(positions, [], right_hip, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, pre_back, right_hip, torso_center)
+    self.send_goal(positions, left_hip, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, back, right_hip, torso_center)
+    self.send_goal(positions, pre_back, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, back, pre_back, torso_center)
+    self.send_goal(positions, back, [], torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, back, back, torso_center)
+    self.send_goal(positions, [], pre_back, torso_center)
+
+    positions = self.get_state()
+    self.send_goal(positions, [], back, torso_center)
 
     # Return to start
     positions = self.get_state()
-    self.send_goal(positions, pre_back, pre_back, torso_center)
+    self.send_goal(positions, [], pre_back, torso_center)
 
     positions = self.get_state()
-    self.send_goal(positions, start, start, torso_center)
+    self.send_goal(positions, [], start, torso_center)
+
+    positions = self.get_state()
+    self.send_goal(positions, pre_back, [], torso_center)
+
+    positions = self.get_state()
+    self.send_goal(positions, start, [], torso_center)
 
     rospy.sleep(4)
 
@@ -175,14 +184,14 @@ class synchronous_traj():
     goal = control_msgs.msg.FollowJointTrajectoryGoal()
 
     # Populates trajectory with joint names.
-    goal.trajectory.joint_names = ['left_joint_1_s','left_joint_2_l','left_joint_3_e','left_joint_4_u', 'left_joint_5_r', 'left_joint_6_b', 'left_joint_7_t','right_joint_1_s','right_joint_2_l','right_joint_3_e','right_joint_4_u', 'right_joint_5_r', 'right_joint_6_b', 'right_joint_7_t','torso_joint_b1','torso_joint_b2']
+    #goal.trajectory.joint_names = ['left_joint_1_s','left_joint_2_l','left_joint_3_e','left_joint_4_u', 'left_joint_5_r', 'left_joint_6_b', 'left_joint_7_t','right_joint_1_s','right_joint_2_l','right_joint_3_e','right_joint_4_u', 'right_joint_5_r', 'right_joint_6_b', 'right_joint_7_t','torso_joint_b1','torso_joint_b2']
 
-    #if len(right_arm) == 7:
-    #    goal.trajectory.joint_names = ['right_joint_s_1','right_joint_2_l','right_joint_3_e','right_joint_4_u', 'right_joint_5_r', 'right_joint_6_b', 'right_joint_7_t']
-    #elif len(left_arm) == 7:
-    #    goal.trajectory.joint_names = ['left_joint_1_s','left_joint_2_l','left_joint_3_e','left_joint_4_u', 'left_joint_5_r', 'left_joint_6_b', 'left_joint_7_t']
-    #else:
-    #    return
+    if len(right_arm) == 7:
+        goal.trajectory.joint_names = ['right_joint_s_1','right_joint_2_l','right_joint_3_e','right_joint_4_u', 'right_joint_5_r', 'right_joint_6_b', 'right_joint_7_t']
+    elif len(left_arm) == 7:
+        goal.trajectory.joint_names = ['left_joint_1_s','left_joint_2_l','left_joint_3_e','left_joint_4_u', 'left_joint_5_r', 'left_joint_6_b', 'left_joint_7_t']
+    else:
+        return
 
 
     # First trajectory point
@@ -192,10 +201,11 @@ class synchronous_traj():
     point2 = trajectory_msgs.msg.JointTrajectoryPoint()
     goal.trajectory.points = [point1, point2]
     #point1.positions = msg_r1.position+msg_r2.position + msg_b1.position + msg_b2.position
+    
     point1.positions = start
-    point1.velocities = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    point1.accelerations = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    point1.effort = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    point1.velocities = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    point1.accelerations = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    point1.effort = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
     goal.trajectory.points[point_index] = point1
     goal.trajectory.points[point_index].time_from_start = rospy.Duration(0.0)
 
@@ -203,11 +213,16 @@ class synchronous_traj():
     point_index += 1
     # This is the actual goal position for the robot, in this case, the random
     # generated position
-    point2.positions = left_arm + right_arm + torso
+    #point2.positions = left_arm + right_arm + torso
+    if len(right_arm) == 7:
+        point2.positions = right_arm
+    elif len(left_arm) == 7:
+        point2.positions = left_arm
+
     point2.positions =  [x/SCALE_MOTION for  x in  point2.positions]
-    point2.velocities = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    point2.accelerations = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    point2.effort = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    point2.velocities = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    point2.accelerations = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    point2.effort = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
     goal.trajectory.points[point_index] = point2
     # TODO: the rospy.Duration(...) implies how faster the robot moves,
     # please check that for the BMDA3. The max for the SDA10F without controller
@@ -221,15 +236,6 @@ class synchronous_traj():
     #rospy.sleep(2.0)
 
   def get_state(self):
-    #msg_r1 = rospy.wait_for_message("/bmda3/bmda3_r1_controller/joint_states", JointState, 5.0)
-    #msg_r2 = rospy.wait_for_message("/bmda3/bmda3_r2_controller/joint_states", JointState, 5.0)
-    #msg_b1 = rospy.wait_for_message("/bmda3/bmda3_b1_controller/joint_states", JointState, 5.0)
-    #msg_b2 = rospy.wait_for_message("/bmda3/bmda3_b2_controller/joint_states", JointState, 5.0)
-    #positions = msg_r1.position+msg_r2.position + msg_b1.position + msg_b2.position
-    
-    #return positions
-
-    # For running in simulation, comment out above statements and uncomment below statements
 
     msg_all = rospy.wait_for_message("/joint_states", JointState, 5.0)
     return msg_all.position
